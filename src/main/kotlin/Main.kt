119c414bb756bb5a1ee667abdc7f6f79fa4fb027
Sergey Kozlov <104564146+kozlovs@users.noreply.github.com>
2022-09-17 12:08:02 +1000

    Добавлены уведомления о новом посте

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -15,16 +15,39 @@ fun main() {
 
     FirebaseApp.initializeApp(options)
 
-    val message = Message.builder()
+    val massageType = MassageType.NEW_POST
+
+    val message = when (massageType) {
+        MassageType.LIKE -> makeLikeMassage()
+        MassageType.NEW_POST -> makeNewPostMassage()
+    }
+
+    FirebaseMessaging.getInstance().send(message)
+}
+
+fun makeLikeMassage(): Message =
+    Message.builder()
         .putData("action", "LIKE")
-        .putData("content", """{
+        .putData(
+            "content", """{
           "userId": 1,
           "userName": "Vasiliy",
           "postId": 2,
           "postAuthor": "Netology"
-        }""".trimIndent())
+        }""".trimIndent()
+        )
         .setToken(token)
         .build()
 
-    FirebaseMessaging.getInstance().send(message)
-}
+fun makeNewPostMassage(): Message =
+    Message.builder()
+        .putData("action", "NEW_POST")
+        .putData(
+            "content", """{
+          "id": 1,
+          "author": "Netology",
+          "content": "Привет! Это новая нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия - помочь встать на путь роста и начать цепочку перемен"
+        }""".trimIndent()
+        )
+        .setToken(token)
+        .build()
---
+++ src/main/kotlin/MassageType.kt
@@ -0,0 +1,5 @@
+package ru.netology.pusher
+
+enum class MassageType {
+    LIKE, NEW_POST
+}
